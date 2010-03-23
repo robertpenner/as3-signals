54fd277e5af32398389422af6c5c4f43c99e2b47
lukebayes <lbayes@patternpark.com>
2010-03-23 08:41:34 -0700

    Updated AsUnit SWC to remove noisy warnings

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'sprout'
 sprout 'as3'
 
-asunit_home = ENV['ASUNIT_HOME'] || File.dirname(__FILE__) + '/../AsUnit4/asunit-4.0'
+asunit_home = ENV['ASUNIT_HOME'] || File.dirname(__FILE__) + '/../AsUnit-bacc/asunit-4.0'
 
 def apply_as3_meta_data_args(t)
   [
@@ -22,15 +22,17 @@ end
 
 mxmlc "bin/AS3SignalsRunner.swf" do |t|
   t.default_size = '1000 500'
-  t.input = 'tests/AllTestsRunner.as'
+  t.input = 'tests/org/osflash/signals/AllTestsRunner.as'
+  #t.allow_source_path_overlap = true
   t.source_path << 'src'
+  t.source_path << 'tests'
   t.library_path << 'libs/asunit4-alpha.swc'
   t.gem_version = '3.3.1'
 end
 
 mxmlc "bin/AS3SignalsAsUnitRunner.swf" do |t|
   t.default_size = '1000 500'
-  t.input = 'tests/AllTestsRunner.as'
+  t.input = 'tests/org/osflash/signals/AllTestsRunner.as'
   t.source_path << 'src'
   t.library_path << "#{asunit_home}/lib/Reflection.swc"
   t.source_path << "#{asunit_home}/src"
